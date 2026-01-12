3e62a1f1f949687a80276e70160989c8056297be
Lucas Steinbach <lucaswsb52@gmail.com>
2026-01-12 20:45:03 -0300

    atualizando migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // usuarios
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_admin')->default(false);
