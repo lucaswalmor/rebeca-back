a56c37b609edcd5c472af195eb2a14fc695dbf20
Lucas Steinbach <lucaswsb52@gmail.com>
2026-01-12 20:53:30 -0300

    melhoria nas seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,14 +13,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::firstOrCreate(
-            ['email' => 'usuario@example.com'],
+        $users = [
             [
+                'email' => 'lucaswsb522@gmail.com',
                 'is_admin' => false,
                 'nome' => 'Lucas',
                 'sobrenome' => 'Steinbach',
                 'apelido' => 'lulu',
-                'email' => 'lucaswsb522@gmail.com',
                 'password' => Hash::make('32329585'),
                 'telefone' => '34992021394',
                 'data_nascimento' => '1993-11-21',
@@ -37,7 +36,37 @@ class UserSeeder extends Seeder
                 'valor_assinatura_semestral' => null,
                 'valor_desconto_trimestral' => null,
                 'valor_desconto_semestral' => null,
-            ]
-        );
+            ],
+            [
+                'email' => 'lucaswsb5222@gmail.com',
+                'is_admin' => false,
+                'nome' => 'Lucas',
+                'sobrenome' => 'Steinbach',
+                'apelido' => 'lulu2',
+                'password' => Hash::make('32329585'),
+                'telefone' => '00000000000',
+                'data_nascimento' => '1990-01-01',
+                'instagram' => null,
+                'tiktok' => null,
+                'facebook' => null,
+                'telegram' => null,
+                'whatsapp' => null,
+                'x_twitter' => null,
+                'privacy' => null,
+                'sobre' => null,
+                'valor_assinatura_mensal' => null,
+                'valor_assinatura_trimestral' => null,
+                'valor_assinatura_semestral' => null,
+                'valor_desconto_trimestral' => null,
+                'valor_desconto_semestral' => null,
+            ],
+        ];
+        
+        foreach ($users as $user) {
+            User::firstOrCreate(
+                ['email' => $user['email']],
+                $user
+            );
+        }
     }
 }
